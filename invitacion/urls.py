73fe7983bbbbbd4d530fe6c1623c1af9f3b89b06
Brandon Vazquez <bhernandez17@lenovo.com>
2023-06-14 20:32:58 -0600

    changes 3

--- invitacion/urls.py
+++ invitacion/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from invitacion import views
 
 app_name = "invitacion"
 
 urlpatterns = [
     path("home/guests/", views.table_guests, name = "table-guests"),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
